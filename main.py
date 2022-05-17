528d922af7048fb37cb6add37bb9a01566528fff
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2022-05-16 20:54:26 -0500

    reorganize reminders.txt

--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import random
 with open('reminders.txt') as f: lines = f.readlines()
 
 # Select a random line from the reminders file.
+lines = [line[1:] for line in lines if line[0] == "-"]
 line = lines[random.randint(0, len(lines) - 1)]
 print(line.strip())
