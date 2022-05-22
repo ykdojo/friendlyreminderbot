7fda952da9cb6963a9dfc2f22ad22c6f48aeb7f6
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2022-05-22 11:55:18 -0500

    change line processing logic

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ def reminder():
      with open('reminders.txt') as f: lines = f.readlines()
 
      # Select a random line from the reminders file.
-     lines = [line[3:] for line in lines if len(line) > 2 and line[2] == "-"]
+     lines = [line.strip() for line in lines if line.strip()]
+     lines = [line[2:] for line in lines if line[0] == '-']
      line = lines[random.randint(0, len(lines) - 1)]
      remind = line.strip()
     
